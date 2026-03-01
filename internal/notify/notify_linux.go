10740f1c1ac295794c0895c8fbbceb2c7cea9b39
mario <mario.perez415@gmail.com>
2026-03-01 13:41:12 -0500

    feat: Use notification icon on linux

--- internal/notify/notify_linux.go
+++ internal/notify/notify_linux.go
@@ -4,8 +4,28 @@ package notify
 
 import (
 	"bufio"
+	"os"
 	"os/exec"
+
+	"github.com/thismarioperez/squrl/assets"
 )
+
+// writeIconTemp writes the embedded notification icon to a temp file and
+// returns its path along with a cleanup function. If writing fails, an empty
+// path and a no-op cleanup are returned.
+func writeIconTemp() (string, func()) {
+	f, err := os.CreateTemp("", "squrl-icon-*.png")
+	if err != nil {
+		return "", func() {}
+	}
+	if _, err := f.Write(assets.NotificationIcon()); err != nil {
+		f.Close()
+		os.Remove(f.Name())
+		return "", func() {}
+	}
+	f.Close()
+	return f.Name(), func() { os.Remove(f.Name()) }
+}
 
 // ShowNotification displays a desktop notification via notify-send.
 // Requires libnotify-bin (or equivalent) to be installed.
@@ -14,7 +34,15 @@ import (
 func ShowNotification(title, message string, onActivate func()) {
 	if onActivate != nil {
 		go func() {
-			cmd := exec.Command("notify-send", "--wait", "--action", "default=Open", title, message)
+			iconPath, cleanup := writeIconTemp()
+			defer cleanup()
+
+			args := []string{"--wait", "--action", "default=Open"}
+			if iconPath != "" {
+				args = append(args, "--icon", iconPath)
+			}
+			args = append(args, title, message)
+			cmd := exec.Command("notify-send", args...)
 			stdout, err := cmd.StdoutPipe()
 			if err != nil {
 				return
@@ -34,5 +62,12 @@ func ShowNotification(title, message string, onActivate func()) {
 		return
 	}
 
-	_ = exec.Command("notify-send", title, message).Run()
+	iconPath, cleanup := writeIconTemp()
+	defer cleanup()
+
+	args := []string{title, message}
+	if iconPath != "" {
+		args = append([]string{"--icon", iconPath}, args...)
+	}
+	_ = exec.Command("notify-send", args...).Run()
 }
