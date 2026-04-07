d758b7d8ccbdb99e2fa6362c833bbc2021bd7483
mario <mario.perez415@gmail.com>
2026-04-07 00:28:47 -0400

    feat: extract scan CLI into standalone squrl-cli binary
    * move scan subcommand logic out of squrl (GUI app) into cmd/squrl-cli/main.go
    * add build-cli and start-cli mise tasks
    * update release workflow to build, package, and upload squrl-cli artifacts for all platforms

--- cmd/squrl/main.go
+++ cmd/squrl/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
-	"context"
-	"errors"
-	"flag"
 	"fmt"
 	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/getlantern/systray"
-	"github.com/thismarioperez/squrl/internal/cli"
 	"github.com/thismarioperez/squrl/internal/logging"
 	"github.com/thismarioperez/squrl/internal/tray"
 )
@@ -22,24 +16,6 @@ func main() {
 	if len(os.Args) == 2 && (os.Args[1] == "--version" || os.Args[1] == "-version") {
 		fmt.Println(version)
 		os.Exit(0)
-	}
-
-	if len(os.Args) >= 2 && os.Args[1] == "scan" {
-		cleanup := logging.Init()
-		defer cleanup()
-
-		opts, err := cli.ParseScanArgs(os.Args[2:])
-		if err != nil {
-			if !errors.Is(err, flag.ErrHelp) {
-				fmt.Fprintf(os.Stderr, "squrl scan: %v\n", err)
-			}
-			os.Exit(2)
-		}
-
-		ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-		defer stop()
-
-		os.Exit(cli.Scan(ctx, opts))
 	}
 
 	cleanup := logging.Init()
